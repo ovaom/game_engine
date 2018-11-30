1a14f60745ebae36f4376129d98fb2778f34346f
Ovaom-server <dev@ovaom.com>
2018-11-30 19:00:40 +0100

    use channel in pygame

--- python/AudioPlay.py
+++ python/AudioPlay.py
@@ -6,27 +6,49 @@ import pygame
 ASSETS_FOLDER = "/home/pi/Documents/ovaom/python/assets"
 
 class AudioPlay(object):
-    
+
     def __init__(self, net):
         pygame.mixer.init()
         self.net = net
         self.instructionsPlaying = False
-        self.instructionsFinished = False
+        self.interrupt = False
 
     def isBusy(self):
         return pygame.mixer.get_busy()
 
-    def playback(self, path):
+    def playback(self, path, callback):
         self.net.sendDspOFF()
         self.sound = pygame.mixer.Sound(path)
-        self.sound.play()
+        pygame.mixer.Channel(0).play(self.sound)
         while self.isBusy():
-            time.sleep(1)
-        self.instructionsFinished = True
-        print "finshed playing"        
+            time.sleep(0.05)
+            if self.interrupt:
+                self.interrupt = False
+                return
+        print "finshed playing"
         self.net.sendDspON()
+        callback()
 
     def stop(self):
         if self.isBusy():
-            self.sound.stop()
+            # self.sound.stop()
+            pygame.mixer.Channel(0).stop()
+            self.interrupt = True
             self.instructionsPlaying = False
+
+class Sample(object):
+    def __init__(self, *args):
+        self.net.sendDspOFF()
+        self.sound = pygame.mixer.Sound(path)
+    
+    def start(self):
+        pygame.mixer.Channel(0).play(self.sound)
+        while self.isBusy():
+            time.sleep(0.05)
+            if self.interrupt:
+                self.interrupt = False
+                return
+        print "finshed playing"
+        self.net.sendDspON()
+        callback()
+    
