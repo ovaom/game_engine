63c7f2b6ad7d34a424de55879ccc7a69b1cf3ae4
Ovaom-server <dev@ovaom.com>
2019-01-08 23:09:45 +0100

    add delay in startup

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
-echo "Waiting 5 seconds before startup..."
+echo "Waiting 3 seconds before startup..." 
+sleep 3
 /usr/bin/pd -nogui -realtime -blocksize 1024 -audiobuf 50 -alsa -r 44100 /home/pi/Documents/puredata/main.pd > /home/pi/Documents/ovaom/logs/puredata_stdout.log 2>&1 & 
+sleep 2
 /usr/bin/python /home/pi/Documents/ovaom/python/game_engine.py > /home/pi/Documents/ovaom/logs/python_stdout.log 2>&1
